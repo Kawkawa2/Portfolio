7e3b8639789a26e2c22a7175662c9fccf37120f8
Kawkawa <121812237+Kawkawa2@users.noreply.github.com>
2024-05-19 21:31:45 +0100

    Update Resume.jsx

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -37,22 +37,21 @@ function Resume() {
             willChange: "transform",
           }}
         >
-          <iframe
-            loading="lazy"
-            style={{
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-              top: 0,
-              left: 0,
-              border: "none",
-              padding: 0,
-              margin: 0,
-            }}
-            src="https://www.canva.com/design/DAFydtYK0Rs/view?embed"
-            allowFullScreen
-            allow="fullscreen"
-          ></iframe>
+          <iframe 
+            loading="lazy" 
+             style="
+             position: absolute; 
+             width: 100%; 
+             height: 100%; 
+             top: 0; 
+             left: 0; 
+             border: none; 
+             padding: 0;
+             margin: 0;"
+            src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGFr1O5DeY&#x2F;DThGJVoJKHwxfAA2Tsv5-A&#x2F;view?embed" 
+             allowfullscreen="allowfullscreen" 
+             allow="fullscreen">
+          </iframe>
         </div>
 
         <Gotolink
